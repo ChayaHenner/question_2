da2322ee67dfb2feb261e030d9818d370f8aa75c
ChayaHenner <chayahenner@gmail.com>
2023-05-11 20:02:46 +0300

    cleaner

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # question_2
+
+Flask Server for COVID Vaccination Data
+This Flask server provides endpoints for retrieving COVID vaccination and diagnosis data of employees in a database. The server has the following endpoints:
+
+/covid_vacs/<id>
+This endpoint retrieves vaccination information of an employee with a specific ID. The response includes the vaccine number, date of vaccination, and manufacturer of each vaccine the employee received, if any.
+
+/covid_dates/<id>
+This endpoint retrieves the date range of positive COVID diagnosis and recovery of an employee with a specific ID.
+
+/get_column/<id>/<request_num>
+This endpoint retrieves a specific column from the employee table in the database. The request_num parameter specifies which column to retrieve, and id specifies the employee ID.
+
+/all/<code>
+This endpoint retrieves all employee data in the database. The code parameter should be set to 8068 to retrieve the data.
+
+/new_employee
+This endpoint is used for adding a new employee to the database. The employee data is passed in the request parameters.
+
+The server uses SQLite to connect to the database and retrieve data. The database path is set to C:\Users\This_user\Desktop\hadassim_project\question_2_update\server.db and can be modified as needed.
+
+Note that the server responds with error messages and appropriate HTTP status codes if invalid data or database errors occur.
--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import sqlite3
 DB_PATH=r'C:\Users\This_user\Desktop\hadassim_project\question_2_update\server.db'
 
 app = Flask(__name__)
-# app.config["DEBUG"]=True
 
 #good        
 @app.route('/covid_vacs/<id>')
@@ -100,7 +99,7 @@ def getAll(code):
 
 
 @app.route('/new_employee', methods=['POST'])
-def post_data_2_2():
+def post_employee():
 
     
     ID = request.args['ID']
@@ -112,23 +111,16 @@ def post_data_2_2():
     DOB= request.args['DOB']
     PHONE = request.args['PHONE']
     CELL_PHONE = request.args['CELL_PHONE']
-    # DATE_POSITIVE = request.args['DATE_POSITIVE']
-    # DATE_RECOVERY = request.args['DATE_RECOVERY']
+    DATE_POSITIVE = request.args['DATE_POSITIVE']
+    DATE_RECOVERY = request.args['DATE_RECOVERY']
     
-<<<<<<< HEAD
    
     conn = sqlite3.connect(DB_PATH)
     c = conn.cursor()
     try:
          if check_values(ID,FIRST_NAME,LAST_NAME,ADDR_CITY,ADDR_ST, ADDR_ST_NUM,DOB,PHONE,CELL_PHONE):
-=======
-    # save the data to the database
-    conn = sqlite3.connect(DB_PATH)
-    c = conn.cursor()
-    try:
-         if check_values(id,firstname,lastname,city,street,streetnum,DOB,phone,cellphone):
->>>>>>> ef95f64d315ead730f53a2ebf10fd3e66ec76f0e
-            c.execute("INSERT INTO EMPLOYEE (id, first_name,last_name,ADDR_CITY,ADDR_ST,ADDR_ST_NUM,DOB,PHONE,CELL_PHONE) VALUES (?,?,?,?,?,?,?,?,?)", (str(id), first_name,LAST_NAME,ADDR_CITY,ADDR_ST,ADDR_ST_NUM,DOB,PHONE,CELL_PHONE))
+            c.execute("INSERT INTO EMPLOYEE (id, first_name,last_name,ADDR_CITY,ADDR_ST,ADDR_ST_NUM,DOB,PHONE,CELL_PHONE) VALUES (?,?,?,?,?,?,?,?,?)", (str(ID), FIRST_NAME,LAST_NAME,ADDR_CITY,ADDR_ST,ADDR_ST_NUM,DOB,PHONE,CELL_PHONE))
+         else: return "entered invalid values" , 405
     except Exception as e:
             conn.commit()
             conn.close()
@@ -138,82 +130,6 @@ def post_data_2_2():
     conn.close()
     return "all good iyH" ,200       
 
-
-<<<<<<< HEAD
-=======
-# @app.route('/employee',methods=['POST'])
-# def postNew():
-#     data=request.json
-#     conn = sqlite3.connect(r'C:\Users\This_user\Desktop\hadassim_project\question_2\server.db')
-#     c = conn.cursor()
-#     id=request.form["id"]
-#     firstname=request.form["firstname"]
-#     lastname=request.form["lastname"]
-#     city=request.form["city"]
-#     street=request.form["street"]
-#     streetnum=request.form["streetnum"]
-#     DOB=request.form["DOB"]
-#     phone=request.form["phone"]
-#     cellphone=request.form["cellphone"]
-    
-#     #c.execute("INSERT INTO EMPLOYEES VALUES(?,?,?,?,?)",(data["id"],data["firstname"],data["lastname"],data["city"],data["street"],data["streetnum"],data["DOB"],data["phone"],data["cellsphone"]))
-    
-#     employee_obj={
-#             "id":id,
-#             "firstname":firstname,
-#             "lastname":lastname,
-#             "city":city,
-#             "street":street,
-#             "streetnum":streetnum,
-#             "DOB":DOB,
-#             "phone":phone,
-#             "cellphone":cellphone
-#     }
-    
-#     if check_values(id,firstname,lastname,city,street,streetnum,DOB,phone,cellphone):
-#         c.execute("INSERT INTO EMPLOYEES VALUES(?,?,?,?,?)",(id,firstname,lastname,city,street,streetnum,DOB,phone,cellphone))
-#     else:
-#         return jsonify("incorrect values") ,404
-#        # rows=c.fetchall()
-#     c.close()
-#     conn.close()
-#     return "data added to database" 
-
-    #post new shot
-
-# app.route('/new_vac',methods=['POST'])
-# def postvac():
-#     data=request.json
-#     id=data.get("id")
-#     conn = sqlite3.connect(DB_PATH)
-#     c = conn.cursor()
-#     c.execute("select MAX(VAC_NUM)  from COVIDVAC WHERE EMPLOYEE_ID = ?",(id,)) #do try catch ,add in order of vac num
-#     num=c.fetchone()[0] #number of vac update it
-#     c.execute("INSERT INTO COVID_VAC VALUES(?,?,?,?)",[data["id"],str(datetime.now())[:19],data["vacman"],num+1])
-#     rows=c.fetchall()
-#     c.close()
-#     conn.close()
-#     return "data added to database",200
-
-
-# app.route('/new_vac',methods=['POST','GET'])
-# def postvac():
-#     return "data added to database",200
-#     # id= request.form["id"]
-#     # vacman= request.form["vacman"]
-#     # conn = sqlite3.connect(DB_PATH)
-#     # c = conn.cursor()
-#     # c.execute("select MAX(VAC_NUM)  from COVIDVAC WHERE EMPLOYEE_ID = ?",(id,)) #do try catch ,add in order of vac num
-#     # num=c.fetchone()[0] #number of vac update it
-#     # c.execute("INSERT INTO COVID_VAC VALUES(?,?,?,?)",(str(id),str(datetime.now())[:19],vacman,num+1,))
-#     # c.close()
-#     # conn.close()
-#     # return "data added to database",200
-    
->>>>>>> ef95f64d315ead730f53a2ebf10fd3e66ec76f0e
-
-    
-#id,firstname,lastname,city,street,streetnum,DOB,phone,cellphone    
     
 #check values r ok
 def check_values(id,firstname,lastname,city,street,streetnum,DOB,phone,cellphone ):
@@ -222,11 +138,7 @@ def check_values(id,firstname,lastname,city,street,streetnum,DOB,phone,cellphone
         correct=False
     return correct
 
-@app.route('/chaya', methods=['POST'])
-def post_data():
-    data = request.get_json()  # get the JSON data from the request
-    # do something with the data, e.g. save to a database
-    return jsonify({'message': 'Data received successfully!'})
+
 
 #good post
 @app.route('/new_vac', methods=['POST'])
@@ -241,11 +153,9 @@ def post_data_2():
     conn = sqlite3.connect(DB_PATH)
     c = conn.cursor()
     c.execute("select MAX(VAC_NUM)  from COVIDVAC WHERE EMPLOYEE_ID = ?",(str(id),)) 
-    #c.execute("select MAX(VAC_NUM)  from COVIDVAC WHERE EMPLOYEE_ID = ?",(str(data["id"]),)) 
 
     num=int(c.fetchone()[0]) #number of vac update it
     c.execute("INSERT INTO COVIDVAC VALUES(?,?,?,?)",(str(id),str(datetime.now())[:10],vacman,num+1,))
-    #c.execute("INSERT INTO COVIDVAC VALUES(?,?,?,?)",(data["id"],str(datetime.now())[:10],data["vacman"],num+1,))
     conn.commit()
     conn.close()
     return "all good iyH" ,200  
